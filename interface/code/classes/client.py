7ff7c18da99d93774d2b80f63b3f2776aa3c1bb8
Alimurat Dinchdonmez <alimrtdinch312@gmail.com>
2024-10-26 10:03:35 -0400

    feat: implementing client-guardian logic

--- interface/code/classes/client.py
+++ interface/code/classes/client.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.abspath(connection_path))
 from connection import load_profile, execute_query
 
 seed_path = "..//..//database//dbt//dbt_snowflake_project//seeds//client.csv"
+underage_client_seed_path = "..//..//database//dbt//dbt_snowflake_project//seeds//underage_client.csv"
 
 def get_last_ID():
 
@@ -19,10 +20,17 @@ def get_last_ID():
 
     return last_seed_ID
 
+def get_last_underage_client_ID():
+
+    last_seed_ID = None
+    with open(underage_client_seed_path , mode='r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            last_seed_ID = int(row['underage_client_id'])  # Get the last ID of the last row
+
+    return last_seed_ID
 
 def add_into_client_seed(new_object):
-    seed_path = "..//..//database//dbt//dbt_snowflake_project//seeds//client.csv"
-    
     # Check if the file exists
     file_exists = os.path.exists(seed_path)
 
@@ -31,39 +39,104 @@ def add_into_client_seed(new_object):
         
         # Write header only if the file is new
         if not file_exists:
-            writer.writerow(['client_id', 'name', 'age', 'isUnderage', 'guardian'])
+            writer.writerow(['client_id', 'name', 'age', 'guardian'])
         
         # Append the new client data directly without adding a newline
         writer.writerow(new_object)  # Append the new client data
+
+def add_into_underage_client_seed(new_object):
+    file_exists = os.path.exists(underage_client_seed_path )
+
+    with open(underage_client_seed_path , mode='a', newline='') as file:
+        writer = csv.writer(file)
+        
+        # Write header only if the file is new
+        if not file_exists:
+            writer.writerow(['underage_client_id', 'cleint_name', 'age', 'guardian', 'relation'])
+        
+        # Append the new client data directly without adding a newline
+        writer.writerow(new_object)  # Append the new client data
+def client_register_message(client_age,):
+
+    print("todo")
+
 
 def create_client():
 
     file_path = "..//..//database//dbt//dbt_snowflake_project//profiles.yml"
     connection = load_profile(file_path)  # Load the YAML profile file
+    
+    underage_client_name = None
 
     print("You have chosen to register as a client.\n")
     
-    client_name = input("Enter your name: ")
     client_age = int(input("Enter your age: "))
-    isUnderage = 'true' if client_age < 18 else 'false'  # Set to 'true' if underage
-    client_guardian = None
-
     # Add the guardian if the client is underage
     if client_age < 18:
-        client_guardian = input("Who is your guardian: ")
 
-    # Set the client_id as appropriate (e.g., fetching the next id from the database)
-    client_id = get_last_ID() +1 # This should be generated based on your logic
+        guardian_age = None
+        sentinel = True
 
-    insert_query = """
-            INSERT INTO client (client_id, name, age, isUnderage, guardian) 
-            VALUES (%s, %s, %s, %s, %s);
-        """
-    execute_query(connection, insert_query, (client_id, client_name, client_age, isUnderage, client_guardian))
+        while(sentinel):
+            guardian_age = int(input("How old is your guardian: "))
 
-    new_object = [client_id, client_name, client_age, isUnderage, client_guardian]
-    print(new_object)
-    add_into_client_seed(new_object)
+            if guardian_age < 18:
+                print("The guardian is not valid. Please enter the age of the valid guardian: ")
+            
+            else:
+                sentinel = False
+
+        underage_client_id = get_last_underage_client_ID() + 1
+        
+        underage_client_name = input("Enter your name: ")
+        guardian_name = input("Who is your guardian: ")
+        guardian = "true"
+
+       
+
+
+        relation_client_guardian = input("What is your relation with your guardian: ")
+        client_id = get_last_ID() + 1
+        
+        insert_query = """
+                INSERT INTO client (client_id, name, age, guardian) 
+                VALUES (%s, %s, %s, %s);
+            """
+        execute_query(connection, insert_query, (client_id, guardian_name, guardian_age, guardian))
+
+        new_object = [client_id, guardian_name, guardian_age, guardian]
+        #print(new_object)
+        add_into_client_seed(new_object)
+
+
+        insert_query = """
+                INSERT INTO underage_client (underage_client_id, client_name,age, guardian, relation) 
+                VALUES (%s, %s, %s, %s, %s);
+            """
+        execute_query(connection, insert_query, (underage_client_id, underage_client_name, client_age, guardian_name, relation_client_guardian))
+        new_underage_client_object = [underage_client_id, underage_client_name, client_age, guardian_name, relation_client_guardian] 
+        add_into_underage_client_seed( new_underage_client_object)
+
+    else:
+
+        client_name = input("Enter your name: ")
+        
+        # Set the client_id as appropriate (e.g., fetching the next id from the database)
+        client_id = get_last_ID() + 1
+        client_guardian = "false"
+        
+        insert_query = """
+                INSERT INTO client (client_id, name, age, guardian) 
+                VALUES (%s, %s, %s, %s);
+            """
+        execute_query(connection, insert_query, (client_id, client_name, client_age, client_guardian))
+
+        new_object = [client_id, client_name, client_age, client_guardian]
+        #print(new_object)
+        add_into_client_seed(new_object)
+
+
+
 
 def test():
      new_object = [1, "Cristiano Ronaldo", 39, 'false', 'null']
--- interface/code/main.py
+++ interface/code/main.py
@@ -5,11 +5,9 @@ connection_path = r"..\..\database\code"  # Ensure this points to the directory 
 sys.path.append(os.path.abspath(connection_path))
 
 from connection import load_profile, execute_query
-from classes.client import create_client,test
+from classes.client import *
 
 def main():
-    # file_path = "..//..//database//dbt//dbt_snowflake_project//profiles.yml"
-    # connection = load_profile(file_path)  # Load the YAML profile file
 
     sentinel = True
     while sentinel:
@@ -26,29 +24,12 @@ def main():
 
         if user_input == "1":
             print("You have chosen to sign in as Administrator.")
-            # TODO: Add your administrator logic here
+           
 
         elif user_input == "2":
+
             create_client()
-            # print("You have chosen to register as a client.\n")
-
-            # client_name = input("Enter your name: ")
-            # client_age = int(input("Enter your age: "))
-            # isUnderage = 'true' if client_age < 18 else 'false'  # Set to 'true' if underage
-            # client_guardian = None
-
-            # # Add the guardian if the client is underage
-            # if client_age < 18:
-            #     client_guardian = input("Who is your guardian: ")
-
-            # # Set the client_id as appropriate (e.g., fetching the next id from the database)
-            # client_id = 3  # This should be generated based on your logic
-
-            # insert_query = """
-            #     INSERT INTO client (client_id, name, age, isUnderage, guardian) 
-            #     VALUES (%s, %s, %s, %s, %s);
-            # """
-            # execute_query(connection, insert_query, (client_id, client_name, client_age, isUnderage, client_guardian))
+       
         elif user_input == "8":
             test()
 
